232455c4f9242e47df8772fb0bd8ba9c3b879aff
BigTaige <827463085@qq.com>
2024-11-26 15:58:03 +0800

    code&dataset

---
+++ data_tools/XML_format_get_spatial_relationship_prediction_data.py
@@ -0,0 +1,320 @@
+import json
+import os
+from PIL import Image
+import random
+from tqdm import tqdm
+import random
+import copy
+from torchvision.ops import box_iou
+import torch
+
+def label_formulate(label):
+    label = label.replace(' ','_')
+    if label == 'On/Off_Switch':
+        label = 'Switch'
+    if label == 'Text_Button':
+        label = 'Button'
+    return label
+
+
+def get_clean_data(json_path):
+    size = get_resolution(json_path.replace('json', 'png'))
+    result = None
+    all_nodes = []
+    with open(json_path, 'r') as f:
+        data = json.load(f)['children']
+        result = extract_components(data)
+
+    def get_all_nodes(data):
+        for node in data:
+            extracted = {
+            'componentLabel': label_formulate(node.get('componentLabel')),
+            'bounds': node.get('bounds'),
+            'iconClass': node.get('iconClass')}
+            all_nodes.append(extracted)
+            
+            if 'children' in node:
+                get_all_nodes(node['children'])
+    
+    get_all_nodes(result)  
+
+    return result, all_nodes
+def judge(bbox1, bbox2, size):
+    a,b,c,d = bbox1[0], bbox1[1], bbox1[2], bbox1[3]
+    A,B,C,D = bbox2[0], bbox2[1], bbox2[2], bbox2[3]
+    w, h = size[0], size[1]
+    for x in [a,b,c,d,A,B,C,D]:
+        if x < 0:
+            return True
+    if a>w or A>w or c>w or C>w or b>h or B>h or d>h or D>h:
+        return True
+    return False
+
+def get_resolution(image_path):
+    with Image.open(image_path) as img:
+        return img.size
+
+def unify_bbox(bbox, size):
+    w, h = size
+    return [bbox[0]/w, bbox[1]/h, bbox[2]/w, bbox[3]/h]
+
+def extract_components(data, parent=None):
+    components = []
+
+    for component in data:
+        extracted = {
+            'componentLabel': label_formulate(component.get('componentLabel')),
+            'bounds': component.get('bounds'),
+            'iconClass': component.get('iconClass')
+        }
+
+        if 'children' in component:
+            extracted['children'] = extract_components(component['children'], extracted)
+        components.append(extracted)
+    return components
+
+def compute_iou(bbox1, bbox2):
+    bbox1 = torch.tensor([bbox1])
+    bbox2 = torch.tensor([bbox2])
+    ious = box_iou(bbox1, bbox2)
+    return ious[0][0]
+
+def get_negatives(node, parent, size, type_='4sibling', all_nodes=None, siblings=None, childrens=None):
+    w, h = size[0], size[1]
+    bbox1, bbox2 = node['bounds'], parent['bounds']
+    s_left, s_top, s_right, s_low = bbox1[0], bbox1[1], bbox1[2], bbox1[3]
+
+    if type_ == '4parent':
+        max_count = 50
+        count = 0
+        while compute_iou([s_left, s_top, s_right, s_low], bbox1) > 0.1 or compute_iou([s_left, s_top, s_right, s_low], bbox2) > 0.3:
+            s_left = random.randint(0, bbox1[0])
+            s_top = random.randint(0, bbox1[1])
+            s_right = random.randint(bbox1[2], w)
+            s_low = random.randint(bbox1[3], h)
+            count = count + 1
+            if count > max_count:
+                return None
+
+        node_new = copy.deepcopy(parent)
+        node_new['bounds'] = [s_left, s_top, s_right, s_low]
+        return ((node, 3, node_new))
+
+    else: # type_ == '4sibling'                    
+        t = random.randint(0, len(all_nodes)-1)
+        relevant_bboxs = [bbox2]
+        for x in siblings:
+            relevant_bboxs.append(x['bounds'])
+        if childrens is not None:
+            for x in childrens:
+                relevant_bboxs.append(x['bounds'])
+        if len(all_nodes) == len(relevant_bboxs):
+            return None
+        max_count = 50
+        count = 0
+        while all_nodes[t]['bounds'] in relevant_bboxs: 
+            t = random.randint(0, len(all_nodes)-1)
+            count = count + 1
+            if count > max_count:
+                return None
+        
+        return ((node, 4, all_nodes[t]))
+    
+
+def parse_json_hierarchy(data, size, num, all_nodes):
+    result = []
+    def dfs(node, parent=None):
+        node_repr = {k: v for k, v in node.items() if k != 'children'}
+   
+        if parent is not None:
+            parent_repr = {k: v for k, v in parent.items() if k != 'children'}
+            result.append((parent_repr, 1, node_repr))
+            result.append((node_repr, 2, parent_repr))
+            for i in range(num):
+                sample = get_negatives(node_repr, parent_repr, size, '4parent')
+                if sample is not None:
+                    result.append(sample) 
+
+        if parent is not None and len(parent['children']) >= 2:
+            siblings = parent['children']
+            parent_repr = {k: v for k, v in parent.items() if k != 'children'}
+            for sibling in siblings:
+                if sibling is not node:
+                    sibling_repr = {k: v for k, v in sibling.items() if k != 'children'}
+                    result.append((node_repr, 0, sibling_repr, parent_repr))
+            for i in range(num):
+                sample = get_negatives(node_repr, parent_repr, size, '4sibling', all_nodes, siblings, node['children'] if 'children' in node else None)
+                if sample is not None:
+                    result.append(sample)
+
+        if 'children' in node:
+            for child in node['children']:
+                if judge(node['bounds'], child['bounds'], size): continue
+                dfs(child, node)
+          
+
+    for entry in data:
+        dfs(entry)
+
+    return result
+
+def unif_bbox(bbox, size_source, scale):
+    w, h = size_source[0], size_source[1]
+    return [int((bbox[0] / w) * scale),
+            int((bbox[1] / h) * scale),
+            int((bbox[2] / w) * scale),
+            int((bbox[3] / h) * scale) ]
+
+def get_relation_prediction_data(data_path, save_path):
+    train_pairs = []
+    Uni_Labels_List = []
+    part = 1
+    a,b,c,d = 0,0,0,0
+    with open(data_path, 'r') as f:
+        data = json.load(f)
+        for img in tqdm(data):
+            sem_json = 'rico_semantic/rico_dataset_v0.1_semantic_annotations/semantic_annotations/' + img.replace('jpg', 'json')
+            if not os.path.exists(sem_json): continue
+
+            clean_data, all_nodes = get_clean_data(sem_json)
+            size = get_resolution('rico_semantic/rico_dataset_v0.1_semantic_annotations/semantic_annotations/' + img.replace('jpg', 'png'))
+
+            output = parse_json_hierarchy(clean_data, size, 2, all_nodes) 
+            
+            for item in output:
+                sample = {}
+                sample['image_path'] = 'dataset/Rico/' + img
+                #size_real = get_resolution(sample['image_path'])
+                scale = 1000
+                node1_bbox = unif_bbox(item[0]['bounds'], size, scale)
+                node1_label = item[0]['componentLabel']
+                node2_bbox = unif_bbox(item[2]['bounds'], size, scale)
+                node2_label = item[2]['componentLabel']
+                node2_class = item[2]['iconClass']
+                relation = item[1]
+
+                if node1_label not in Uni_Labels_List:
+                    Uni_Labels_List.append(node1_label)
+                if node2_label not in Uni_Labels_List:
+                    Uni_Labels_List.append(node2_label)
+              
+                if node1_bbox == node2_bbox: continue
+
+                node1_type = node1_label if node1_label != '' else node1_class
+                node2_type = node2_label if node2_label != '' else node2_class
+                n1 = random.randint(0, len(QUESTION_PROMPT_FOR_RELATION_PREDICTION)-1)
+                n2 = random.randint(0, len(SIBLING_RELATIONSHIP_ANSWER_PROMPT_FOR_RELATION_PREDICTION)-1)
+                n3 = random.randint(0, len(CONTAINMENT_RELATIONSHIP_ANSWER_PROMPT_FOR_RELATION_PREDICTION)-1)
+                n4 = random.randint(0, len(NO_CONTAINMENT_RELATIONSHIP_ANSWER_PROMPT_FOR_RELATION_PREDICTION)-1)
+                n5 = random.randint(0, len(OTHERS)-1)
+
+                question = QUESTION_PROMPT_FOR_RELATION_PREDICTION[n1].format(node1_bbox, node2_bbox)
+
+                if relation == 0:
+                    a += 1
+                    parent_bbox = unif_bbox(item[-1]['bounds'], size, scale)
+                    parent_type = item[-1]['componentLabel']
+                    assert (parent_type != None) and (parent_type != '')
+                    answer = SIBLING_RELATIONSHIP_ANSWER_PROMPT_FOR_RELATION_PREDICTION[n2].format(
+                        bbox1=node1_bbox, type1=node1_type, bbox2=node2_bbox, type2=node2_type, bbox3=parent_bbox, type3=parent_type)
+
+                elif relation == 1 or relation == 2:
+                    b += 1
+                    if relation == 1: node1_bbox, node1_type, node2_bbox, node2_type = node2_bbox, node2_type, node1_bbox, node1_type
+                    answer = CONTAINMENT_RELATIONSHIP_ANSWER_PROMPT_FOR_RELATION_PREDICTION[n3].format(
+                        bbox1=node1_bbox, type1=node1_type, bbox2=node2_bbox, type2=node2_type)
+                
+                elif relation == 3:
+                    c += 1
+                    answer = NO_CONTAINMENT_RELATIONSHIP_ANSWER_PROMPT_FOR_RELATION_PREDICTION[n4].format(
+                        bbox1=node1_bbox, type1=node1_type, bbox2=node2_bbox)
+
+                elif relation == 4:
+                    d += 1
+                    answer = OTHERS[n5].format(bbox1=node1_bbox, type1=node1_type, bbox2=node2_bbox, type2=node2_type)
+
+                sample['question'] = question
+                sample['answer'] = answer
+
+                train_pairs.append(sample)
+       
+                if len(train_pairs) == 100000:
+                    random.shuffle(train_pairs)
+                    print(f'### Saving {(part-1)*100000}-{part*100000} samples.......')
+                    with open(save_path + f'{part}_' + 'relation_train.json', 'w') as f:
+                        f.write(json.dumps(train_pairs))
+                    train_pairs = []
+                    part += 1 
+    count = len(train_pairs)+ (part-1)*100000
+
+    print(f'label=0:{a/count}, label=1,2:{b/count}, label=3:{c/count}, label=4:{b/count}')
+    print(f'full num: {count}')
+    print(f'{a}  {b}  {c}  {d}')
+    print(Uni_Labels_List)
+
+    random.shuffle(train_pairs)      
+    with open(save_path+ f'{part}_' + 'relation_train.json', 'w') as f:
+        f.write(json.dumps(train_pairs))
+
+
+
+QUESTION_PROMPT_FOR_RELATION_PREDICTION = [
+    "Output the relationship between the elements at {} and {}.(with XML format)",
+    "How are the items situated in {} and {} related to one another?(with XML format)",
+    "How do the elements in {} and {} relate to each other?(with XML format)",
+    "There are two components at {} and {}. What is their relationship?(with XML format)",
+    "What is the relationship between the item at {} and {}?(with XML format)"
+]
+# label=0
+SIBLING_RELATIONSHIP_ANSWER_PROMPT_FOR_RELATION_PREDICTION = [
+    "Related:\n\
+<{type3}>\n\
+    <box>{bbox3}</box>\n\
+    <{type1}>\n\
+        <box>{bbox1}</box>\n\
+    </{type1}>\n\
+    <{type2}>\n\
+        <box>{bbox2}</box>\n\
+    </{type2}>\n\
+</{type3}>"
+]   
+#label=1, 2
+CONTAINMENT_RELATIONSHIP_ANSWER_PROMPT_FOR_RELATION_PREDICTION = [
+    "Related:\n\
+<{type2}>\n\
+    <box>{bbox2}</box>\n\
+    <{type1}>\n\
+        <box>{bbox1}</box>\n\
+    </{type1}>\n\
+</{type2}>"
+]
+#label=3
+NO_CONTAINMENT_RELATIONSHIP_ANSWER_PROMPT_FOR_RELATION_PREDICTION = [
+    "Unrelated:\n\
+<{type1}>\n\
+    <box>{bbox1}</box>\n\
+</{type1}>,\n\
+<Unknown>\n\
+    <box>{bbox2}</box>\n\
+</Unknown>"
+]
+# no direct spatial relationship
+OTHERS = [
+    "Unrelated:\n\
+<{type1}>\n\
+    <box>{bbox1}</box>\n\
+</{type1}>,\n\
+<{type2}>\n\
+    <box>{bbox2}</box>\n\
+</{type2}>"
+]
+
+if __name__ == '__main__':
+    save_path = 'SpatialRelationPrediction/'
+    get_relation_prediction_data('dataset/rico_sem_train_index.json', save_path)
+
+
+
+
+
+
---
+++ data_tools/get_small_icon_grounding_data.py
@@ -0,0 +1,146 @@
+import json
+import os
+import random
+from tqdm import tqdm
+ICON_TYPE = {
+    'ICON_SEND': ['Send Icon', 'Used to send messages or data'],
+    'ICON_CALL': ['Call Icon', 'Initiates a phone call'],
+    'ICON_STAR': ['Star Icon', 'Marks an item as a favorite'],
+    'ICON_PAUSE': ['Pause Icon', 'Pauses a current activity'],
+    'ICON_THREE_DOTS': ['Three Dots Icon', 'Indicates more options'],
+    'ICON_CALENDAR': ['Calendar Icon', 'Displays calendar events'],
+    'ICON_MIC': ['Microphone Icon', 'Used for audio input'],
+    'ICON_PLUS': ['Plus Icon', 'Adds a new item or feature'],
+    'ICON_PEOPLE': ['People Icon', 'Represents users or contacts'],
+    'ICON_SAD_FACE': ['Sad Face Icon', 'Represents a sad face symbol'],
+    'ICON_LAUNCH_APPS': ['Launch Apps Icon', 'Opens an app drawer'],
+    'ICON_FACEBOOK': ['Facebook Icon', 'Links to Facebook platform'],
+    'ICON_VOLUME_STATE': ['Volume State Icon', 'Indicates audio volume status'],
+    'ICON_HEART': ['Heart Icon', 'Represents health, love or favorites'],
+    'ICON_TIME': ['Time Icon', 'Displays time-related information'],
+    'ICON_CLOUD': ['Cloud Icon', 'Indicates weather, cloud storage or services'],
+    'ICON_ARROW_BACKWARD': ['Backward Arrow Icon', 'Navigates to the previous screen'],
+    'ICON_SHARE': ['Share Icon', 'Sharing content with others'],
+    'ICON_GOOGLE': ['Google Icon', 'Links to Google services'],
+    'ICON_SETTINGS': ['Settings Icon', 'Opens configuration options'],
+    'ICON_NAV_BAR_CIRCLE': ['Nav Bar Circle Icon', 'Indicates navigation button'],
+    'ICON_V_UPWARD': ['Upward Icon', 'Indicates upward movement or scroll'],
+    'ICON_MIC_MUTE': ['Muted Microphone Icon', 'Indicates audio input is muted'],
+    'ICON_V_FORWARD': ['Forward Icon', 'Indicates forward movement or action'],
+    'ICON_PAPERCLIP': ['Paperclip Icon', 'Used to attach files'],
+    'ICON_ENVELOPE': ['Envelope Icon', 'Indicates email or message'],
+    'ICON_GALLERY': ['Gallery Icon', 'Opens photo or image collection'],
+    'ICON_VIDEOCAM': ['Video Camera Icon', 'Used for video capture'],
+    'ICON_CHAT': ['Chat Icon', 'Initiates a chat conversation'],
+    'ICON_ASSISTANT': ['Assistant Icon', 'Represents a virtual assistant'],
+    'ICON_LIST': ['List Icon', 'Displays a list view'],
+    'ICON_HOME': ['Home Icon', 'Navigates to the home screen'],
+    'ICON_MAGNIFYING_GLASS': ['Magnifying Glass Icon', 'Indicates search functionality'],
+    'ICON_NOTIFICATIONS': ['Notifications Icon', 'Displays alerts and notifications'],
+    'ICON_PLAY': ['Play Icon', 'Starts media playback'],
+    'ICON_LOCATION': ['Location Icon', 'Indicates geographical location'],
+    'ICON_STOP': ['Stop Icon', 'Stops current activity'],
+    'ICON_INFO': ['Info Icon', 'Displays information'],
+    'ICON_V_BACKWARD': ['Backward Icon', 'Indicates backward movement or action'],
+    'ICON_NAV_BAR_RECT': ['Nav Bar Rect Icon', 'Represents navigation button'],
+    'ICON_X': ['Close Icon', 'Closes a window or dialog'],
+    'ICON_CHECK': ['Checkmark Icon', 'Indicates selection completion'],
+    'ICON_V_DOWNWARD': ['Downward Icon', 'Indicates downward movement or scroll'],
+    'ICON_SUN': ['Sun Icon', 'Indicates screen mode or brightness'],
+    'ICON_PERSON': ['Person Icon', 'Represents a user or profile'],
+    'ICON_THREE_BARS': ['Three Bars Icon', 'Represents a menu or navigation'],
+    'ICON_TAKE_PHOTO': ['Take Photo Icon', 'Initiates taking a photo'],
+    'ICON_ARROW_FORWARD': ['Forward Arrow Icon', 'Navigates to the next screen'],
+    'ICON_REFRESH': ['Refresh Icon', 'Reloads the current page or content'],
+    'ICON_DOWNLOAD': ['A downward arrow', 'Initiates a download process'],
+    'ICON_HISTORY': ['A clock symbol representing past records', "To view history or past actions"],
+    'ICON_ARROW_DOWNWARD':['Arrow Downward Icon', 'Indicates downward movement or scroll with an arrow symbol'],
+    'ICON_QUESTION': ['Question mark icon', 'Symbolizes a question or inquiry about information'],
+    'ICON_COMPASS': ["Find direction or navigation", "Indicates navigation or locating purpose"],
+    'ICON_EXPAND': ["Expand Icon", "Used to expand a menu or section"],
+    'ICON_ARROW_UPWARD': ["An upward-pointing arrow symbol", "Used to indicate upward movement or action"],
+    'ICON_SHOPPING_CART': ["A cart filled with goods", "Used to represent shopping or purchases"],
+    'ICON_HOME': ["Home Icon", "Back to the main screen"],
+    'ICON_THUMBS_UP': ["Thumbs Up Icon", "Indicates approval or like"],
+    'ICON_CAST': ["Cast Icon", "Used to cast content to other devices"],
+    'ICON_END_CALL': ["End Call Icon", "Ends an ongoing phone call"],
+    'ICON_UPLOAD': ["Upload Icon", "Initiates file upload"],
+    'ICON_HAPPY_FACE': ["Happy Face Icon", "Represents a smiley face symbol"]
+}
+def process_bbox(bbox, w, h):
+    #y,x,h,w
+    new_bbox = [ int(bbox[1]*w), int(bbox[0]*h), int((bbox[1]+bbox[3])*w), int((bbox[0]+bbox[2])*h)]
+    return new_bbox #,x1,y1,x2,y2
+
+def cal_size(bbox):
+    a, b, c, d = bbox[0], bbox[1], bbox[2], bbox[3] 
+    return (c - a) * (d - b)
+
+def cal_percent(bbox, image_size):
+    return (bbox / image_size) * 100 
+
+def for_minicpm(box,w,h):
+    return [box[0]*1000//w, box[1]*1000//h, box[2]*1000//w, box[3]*1000//h]
+
+def get_folds(fold='', save_path='', max_rate=0.3):
+    types = []
+    part = 0
+    train_items = []
+    dirs = os.listdir(fold) 
+    for dir_ in tqdm(dirs):
+        # if dir_ in ['google_apps3', 'google_apps4', 'google_apps1', 'google_apps2']: continue 
+        files = os.listdir(fold+dir_) 
+        for file in files:
+            path = fold + dir_ + '/' + file + '/' + 'meta.json'
+            with open(path, 'r') as f:
+                data = json.load(f)
+                for row in data:
+                
+                    item = {}
+                    item['image_path'] = row['image_path']
+                    bbox_list = row['image/ui_annotations_positions']
+                    item['orc_list'] = row['image/ui_annotations_text']
+                    item['type_list'] = row['image/ui_annotations_ui_types']
+                    map_ = {}
+                    for i, x in enumerate(item['type_list']):
+                        if x in map_:
+                            map_[x].append(bbox_list[i])
+                        else:
+                            map_[x] = [bbox_list[i]]
+                    for x in map_:
+                        if len(map_[x]) == 1 and (x not in ['TEXT']):
+                            sample = {}
+                            sample['image_path'] = item['image_path'] 
+                            w, h =  row['image/width'], row['image/height']
+                            sample['resolution'] = [w, h]
+                            sample['gt'] = process_bbox(map_[x][0], w, h)
+                            sample['type'] = x
+
+                            source_bbox = cal_size(sample['gt'])
+                            rate = cal_percent(source_bbox, w * h)
+                            sample['rate'] = rate
+                            sample['gt'] = for_minicpm(sample['gt'], w, h)
+
+                            if rate <= 0. or rate >= max_rate: continue
+                            else:
+                                if x not in ICON_TYPE: continue
+                                else:
+                                    question = f'Give the bounding box reffering to "{ICON_TYPE[x][0].lower()}".'
+                                    sample['question'] = question
+                                    train_items.append(sample)
+
+                        if len(train_items) == 100000:
+                            random.shuffle(train_items)
+                            with open(save_path + f'AITW_{part}_small_icon_train.json', 'w') as f:
+                                f.write(json.dumps(train_items))
+                            part += 1
+                            train_items = []
+    random.shuffle(train_items)
+    with open(save_path + f'AITW_{part}_small_icon_train.json', 'w') as f:
+        f.write(json.dumps(train_items))
+    print(f'full nums:{len(train_items)+ (part-1)*100000}')
+
+if __name__ == '__main__':
+    get_folds(fold='AITW_data_path/', 
+                save_path='saving_path/', max_rate=0.3)
+
