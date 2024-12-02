5cf578ae69502cf6d6ccc8365cd3d0055ffb20f8
“BigTaige” <shengchu.sc@antgroup.com>
2024-12-02 14:13:16 +0800

    ‘code

---
+++ model/shell/multi_step_training/benchmark.sh
@@ -0,0 +1,73 @@
+set -x
+
+GPUS=${GPUS:-8}
+BATCH_SIZE=${BATCH_SIZE:-64}
+PER_DEVICE_BATCH_SIZE=${PER_DEVICE_BATCH_SIZE:-8}
+GRADIENT_ACC=$((BATCH_SIZE / PER_DEVICE_BATCH_SIZE / GPUS))
+
+
+export PYTHONPATH="${PYTHONPATH}:$(pwd)"
+export MASTER_PORT=34229
+export TF_CPP_MIN_LOG_LEVEL=3
+export LAUNCHER=pytorch
+
+OUTPUT_DIR='ckpts/benchmark'
+
+if [ ! -d "$OUTPUT_DIR" ]; then
+  mkdir -p "$OUTPUT_DIR"
+fi
+
+
+nohup torchrun \
+  --nnodes=1 \
+  --node_rank=0 \
+  --master_addr=127.0.0.1 \
+  --nproc_per_node=${GPUS} \
+  --master_port=${MASTER_PORT} \
+  internvl/train/MPGUI_finetune.py \
+  --use_mpgui True \
+  --train_graphical_perceiver False \
+  --train_textual_perceiver False \
+  --train_spatial_perceiver False \
+  --train_fusion_gate False \
+  --use_fusion_gate True \
+  --model_name_or_path "ckpts/train_fusion_gate/merged_ckpts" \
+  --conv_style "internlm2-chat" \
+  --output_dir ${OUTPUT_DIR} \
+  --meta_path "training_data/benchmark_training_data.json" \
+  --overwrite_output_dir True \
+  --force_image_size 448 \
+  --max_dynamic_patch 4 \
+  --down_sample_ratio 0.5 \
+  --drop_path_rate 0.1 \
+  --freeze_llm True \
+  --freeze_mlp True \
+  --freeze_backbone True \
+  --use_llm_lora 64 \
+  --use_backbone_lora 64 \
+  --vision_select_layer -1 \
+  --dataloader_num_workers 4 \
+  --bf16 True \
+  --num_train_epochs 1 \
+  --per_device_train_batch_size ${PER_DEVICE_BATCH_SIZE} \
+  --gradient_accumulation_steps ${GRADIENT_ACC} \
+  --evaluation_strategy "no" \
+  --save_strategy "epoch" \
+  --save_steps 200 \
+  --save_total_limit 1 \
+  --learning_rate 4e-5 \
+  --weight_decay 0.01 \
+  --warmup_ratio 0.03 \
+  --lr_scheduler_type "cosine" \
+  --logging_steps 1 \
+  --max_seq_length 4096 \
+  --do_train True \
+  --grad_checkpoint True \
+  --group_by_length True \
+  --dynamic_image_size True \
+  --use_thumbnail True \
+  --ps_version 'v2' \
+  --deepspeed "zero_stage1_config.json" \
+  --report_to "tensorboard" \
+  > "${OUTPUT_DIR}/training_log.txt" \
+  2>&1 &
